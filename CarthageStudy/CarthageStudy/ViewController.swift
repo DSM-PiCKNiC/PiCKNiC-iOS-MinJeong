3453a687da7b3f5e871f119c76ef9bc808132a9f
jihoon <leejh080628@naver.com>
2025-04-14 22:56:55 +0900

    Carthage 예제코드

---
+++ CarthageStudy/CarthageStudy/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  CarthageStudy
+//
+//  Created by 이지훈 on 4/14/25.
+//
+
+import UIKit
+import SnapKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
