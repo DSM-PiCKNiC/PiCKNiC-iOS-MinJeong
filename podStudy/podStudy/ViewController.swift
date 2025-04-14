2a2ceb2174773e77f13c9e2c1a1eabc8821b5ca4
jihoon <leejh080628@naver.com>
2025-04-14 22:57:34 +0900

    코코아팟 예제코드

---
+++ podStudy/podStudy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  podStudy
+//
+//  Created by 이지훈 on 4/14/25.
+//
+
+import UIKit
+import Alamofire
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
